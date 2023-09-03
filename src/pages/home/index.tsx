06f1da5b4dc3eb6381e6b20ab4982d43bd65cd98
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-09-03 10:08:41 -0300

    SEO optimizations; Change Database URL connection to deploy db

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,27 +4,36 @@ import Image from 'next/image'
 
 import previewImage from '../../assets/app-preview.png'
 import { ClaimUsernameForm } from './components/ClaimUsername'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
-        <ClaimUsernameForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImage}
-          height={400}
-          quality={100}
-          priority
-          alt="Calnedário simbolizando a aplicação"
-        />
-      </Preview>
-    </Container>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre."
+      />
+
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+          <ClaimUsernameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calnedário simbolizando a aplicação"
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
